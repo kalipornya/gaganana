3bc86ed8767f589e4f36b19918cca1dc7b5777a4
kalipornya <42149913+kalipornya@users.noreply.github.com>
2018-08-07 12:42:20 +0800

    Update 20180725211427_create_twilio_credentials.rb

--- db/migrate/20180725211427_create_twilio_credentials.rb
+++ db/migrate/20180725211427_create_twilio_credentials.rb
@@ -1,6 +1,6 @@
 class CreateTwilioCredentials < ActiveRecord::Migration[5.2]
   def change
-    create_table :twilio_credentials do |t|
+    create_table :credentials do |t|
       t.string :account_sid
       t.string :auth_token
       t.timestamps
